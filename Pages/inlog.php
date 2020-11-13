043537fbd647cd165522fae50029b2121d5cd62b
Chris020103 <Chrism@caiway.net>
2020-11-13 11:03:51 +0100

    Login.php veranderen naar Bootstrap

--- Pages/inlog.php
+++ Pages/inlog.php
@@ -44,7 +44,7 @@ require '../config.php';
 <!--        </ul>-->
 <!--    </div>-->
 
-<nav class="navbar navbar-expand-lg ">
+<nav class="navbar navbar-expand-lg  ">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -53,10 +53,10 @@ require '../config.php';
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a  href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a  href="#">Link</a>
+                <a class="nav-link" href="#">Link</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
