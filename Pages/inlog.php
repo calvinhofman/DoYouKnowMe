8ec3d0a25f6d2b46e3ec58c99a00d10a0e09c7b9
Martijn <kalkmanmartijn@gmail.com>
2020-11-13 09:51:24 +0100

    test

--- Pages/inlog.php
+++ Pages/inlog.php
@@ -24,7 +24,7 @@ require '../config.php';
                 <a href="http://">Profile</a>
             </li>
             <li class="navItem">
-                <a href="http://">friends</a>
+                <a href="http://">TEST</a>
             </li>
             <!-- <li class="navItem">
                 <a href="http://">log in</a>
