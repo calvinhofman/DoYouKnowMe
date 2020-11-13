ab6c9c67bd241486f296e131016e6a431e4d1864
Chris020103 <Chrism@caiway.net>
2020-11-13 11:06:16 +0100

    Login.php veranderen naar Bootstrap

--- Pages/inlog.php
+++ Pages/inlog.php
@@ -44,6 +44,9 @@ require '../config.php';
 <!--        </ul>-->
 <!--    </div>-->
 
+
+
+<!--Navbar-->
 <nav class="navbar navbar-expand-lg  ">
     <a class="navbar-brand" href="#">Ken je mij?</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
