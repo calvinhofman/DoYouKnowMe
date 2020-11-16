d0977e50697715d1e1623725e0b61e52349d7f1d
Chris020103 <Chrism@caiway.net>
2020-11-16 21:18:46 +0100

    Index.php leeg gemaakt en veranderd naar registeren.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+ require 'Pages/navbar.php'
+?>
+
+<html>
+<head>
+    <!--    Favicon-->
+    <link rel="icon" href="../Images/Favicon.jpg" sizes="16x16" type="image/jpg">
+
+    <!--CSS link-->
+    <link rel="stylesheet" href="Styling/style.css">
+    <link rel="stylesheet" href="Styling/index.css" type="text/css">
+    <!--Meta links-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--    Bootstrap 4.0-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--Catchpa-->
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+
+    <!--    W3 css-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Ken je mij?</title>
+</head>
+
+</head>
+<body>
+<div class="main-picture col-md-12"></div>
+</body>
+</html>
