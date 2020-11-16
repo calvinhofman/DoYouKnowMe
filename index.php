b02a1954914f68228f7d413123f60e1813c7d2c3
Chris020103 <Chrism@caiway.net>
2020-11-16 22:15:35 +0100

    Index.php aangepakt en gemaakt met Bootstrap moet er nog aan verder.

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
- require 'Pages/navbar.php'
-?>
 
-<html>
+<html lang="nl">
 <head>
     <!--    Favicon-->
-    <link rel="icon" href="../Images/Favicon.jpg" sizes="16x16" type="image/jpg">
+    <link rel="icon" href="Images/Favicon.jpg" sizes="16x16" type="image/jpg">
 
     <!--CSS link-->
     <link rel="stylesheet" href="Styling/style.css">
@@ -25,8 +22,82 @@
     <title>Ken je mij?</title>
 </head>
 
-</head>
 <body>
-<div class="main-picture col-md-12"></div>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light" style="  background-color: #18072C;">
+    <a class="navbar-brand" style="color: white" href="#">Ken je mij?</a>
+    <button class="navbar-toggler" type="button" style="background-color: white" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link style='color:white'" href="Pages/inlog.php">Inloggen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link style='color:white'" href="Pages/home.php">Registeren</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--Einde Navbar-->
+
+
+
+
+    <img src="Images/Ken-je-mij.jpg" class="img-fluid" style="width: 100%" alt="Responsive image">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm">
+                <div class="col-sm">
+                    <img src="Images/Vrienden-kennis.jpg" class="img-fluid img-thumbnail img-responsive" alt="Hoe goed ken jij je vrienden?">
+                        <h3 class="text-align">Het spel om te spelen met je vrienden. Ken je je vrienden zo goed als dat je zegt? Test het uit in het meest vernieuwde spel van allemaal "KEN JE MIJ?"</h3>
+
+                </div>
+            </div>
+            <div class="col-sm border-col">
+                <h1 class="text-align">Ken jij mij eigenlijk wel?</h1>
+                <h3 class="text-align">Wil jij nou jouw geliefde, getrouwde, of verloofde uittesten? Probeer "Ken je mij?" Het spel om uit te testen wie elkaar het beste kent!</h3>
+                <img src="Images/Verloofde.jpg" class="img-fluid img-thumbnail img-responsive" alt="Hoe goed ken jij je verloofde?">
+
+            </div>
+            <div class="col-sm">
+                <img src="Images/Familie.png" class="img-fluid img-thumbnail img-responsive" alt="Hoe goed ken je jouw gezin?">
+                <h3 class="text-align">Heb jij een hecht gezin, en wil je testen wie elkaar het beste kent? Of leef jij in een hecht gezin check of je jouw ouders broer/zus wel echt zo goed ken!</h3>
+            </div>
+        </div>
+    </div>
+
+
+<!-- Footer -->
+<footer class="page-footer font-small  pt-4">
+
+    <!-- Footer Elements -->
+    <div class="container">
+
+        <!-- Call to action -->
+        <ul class="list-unstyled list-inline text-center py-2">
+            <li class="list-inline-item">
+                <h5 class="mb-1">Register for free</h5>
+            </li>
+            <li class="list-inline-item">
+                <a href="#!" class="btn btn-outline-white btn-rounded">Sign up!</a>
+            </li>
+        </ul>
+        <!-- Call to action -->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="py-3">
+<!--        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>-->
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
 </body>
 </html>
