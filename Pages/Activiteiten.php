2b13d0a9e94c79b0b83897e03ef2f1b938c8fea0
vincent benders <vincentbenders@gmail.com>
2020-11-17 13:43:44 +0100

    gestart met activiteiten.php

---
+++ Pages/Activiteiten.php
@@ -0,0 +1,38 @@
+<?php
+require '../config.php';
+
+$query = "SELECT * FROM fabels_en_feiten"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="./Images/background.jpg">
+<div class="Nav">
+        <ul class="navBar-nav" id="ToggleNav">
+            <li class="navItem Active">
+                <a href="http://">Home</a>
+            </li>
+            <li class="navItem">
+                <a href="http://">Profile</a>
+            </li>
+            <li class="navItem">
+                <a href="http://">Friends</a>
+            </li>
+    </div>
+<div>
+    <script>
+    var x ="", i;
+    for (i=1; i<=6; i++){
+        <?php ?>
+    }
+    </script>
+</div>
+</body>
+</html>
